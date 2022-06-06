fb88ecacbc9215c9b9f2bd24939c9ff3f9bdc68e
Susan Minott <susan_minott@yahoo.com>
2022-06-05 23:29:20 -0400

    commit tree b28a152407ca8690723408a267114131d7cebad3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
 # Your code here!
+
+#Code 1
+def greet_programmer ()
+    puts "Hello, programmer!"
+    #"Hello, programmer!"
+end
+
+#Code 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet 'Naureen'
+
+#Code 3a; Default
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+#Code 3b; Default
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default 'Sunny'
+
+
+#Code 4
+def add (num1, num2)
+    return num1 + num2
+end
+add(1,2)
+
+#Code 5
+def halve(number)
+    if number == integer
+        number / 2
+    
+    else 
+        return nil
+    end
+end
+halve(20)
+
+
